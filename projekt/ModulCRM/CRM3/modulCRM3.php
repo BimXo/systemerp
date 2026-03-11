eb60d8e3f07d42c149e8fa6e95f61c77b34286f1
Mikołaj <mikolaj.zsti@gmail.com>
2026-03-11 09:35:30 +0100

    Add files via upload

---
+++ projekt/ModulCRM/CRM3/modulCRM3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opcja 3</title>
+</head>
+<body>
+    <form method="post" action="modulCRM3submit.php">
+        <input type="number" name="idih" required>
+        <input type="submit" value="wyszukaj">
+</body>
+</html>
